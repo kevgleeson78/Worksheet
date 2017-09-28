6c2c13a1f31458b8c540caa56bed9b3b86db318f
Kevin@test.com <kevin@test.com>
2017-09-29 00:56:55 +0100

    merge function changed

--- merge.go
+++ merge.go
@@ -5,16 +5,19 @@ import (
 	"sort"
 )
 
-func merge(list1 []int, list2 []int) (int, error) {
-	sort.Ints(list1)
-	sort.Ints(list2)
-	list3 := append(list1, list2...)
-
-	return fmt.Println(list3)
-}
-func main() {
+func merge() (list3 []int) {
 	list1 := []int{22, 18, 1, 78, 99, 4}
 	list2 := []int{11, 32, 41, 5, 204, 71, 2}
-	merge(list1, list2)
+	sort.Ints(list1)
+	sort.Ints(list2)
+	list3 = append(list1, list2...)
+
+	return list3
+}
+func main() {
+
+	result := merge()
+
+	fmt.Println(result)
 
 }
