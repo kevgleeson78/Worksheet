68ea545272622a61aef04b1a9fd815aa7e3d6f5b
Kevin@test.com <kevin@test.com>
2017-09-30 19:33:00 +0100

    factoral code tided up comments added

--- factoral.go
+++ factoral.go
@@ -1,25 +1,41 @@
+//Problem Sheet 4
+//Factoral
+//Author: Kevin Gleeson
+//URL: https://github.com/kevgleeson78/Worksheet
+//Source:
+//https://stackoverflow.com/questions/11270547/go-big-int-factorial-with-recursion
+//Date 30/09/17
+
 package main
 
+// import fmt and math/big
 import (
 	"fmt"
 	"math/big"
 )
 
+// factoral function using BigInts
 func factoral(n uint64) (r *big.Int) {
-
+	//declare two variables bn from function input
 	one, bn := big.NewInt(1), new(big.Int).SetUint64(n)
-
+	//declare r for multiplying in loop
+	//and returning to second function
 	r = big.NewInt(1)
-	if bn.Cmp(one) <= 0 {
-		return
-	}
-	for i := big.NewInt(2); i.Cmp(bn) <= 0; i.Add(i, one) {
+	//for loop different to standard
+	//i:=1 ; i <= bn; i++;
+	//when is  == bn the loop will stop
+	for i := big.NewInt(1); i.Cmp(bn) <= 0; i.Add(i, one) {
+		//multiply r * i store in r for next itteration
+		//until condition is met
 		r.Mul(r, i)
 	}
+	//return final result
 	return
 }
 
+//function for adding the result of the factoral function
 func add(number *big.Int) *big.Int {
+	//three variables
 	ten := big.NewInt(10)
 	sum := big.NewInt(0)
 	mod := big.NewInt(0)
@@ -31,6 +47,6 @@ func add(number *big.Int) *big.Int {
 	return sum
 }
 func main() {
-	fmt.Println(add(factoral(100)))
+	fmt.Println(add(factoral(10)))
 
 }
